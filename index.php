9b2894b2c585ca2eb1ee14df2a638e0042a08f4a
Tri Vuong <tvuongjr@gibraltar1.com>
2025-10-21 14:49:03 -0500

    added test mail() function call

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
 echo "Hello Azure!";
+
+
+$to = "tvuongjr@gibraltar1.com"; // Recipient's email address
+$subject = "Test Email from PHP Azure"; // Subject of the email
+$message = "Hello, this is a test email sent using the PHP mail() function."; // Email body
+$headers = "From: tvuongjr@gibraltar1.com"; // Sender's email address
+
+// Send the email
+if (mail($to, $subject, $message, $headers)) {
+    echo "Email sent successfully!";
+} else {
+    echo "Email sending failed.";
+}
